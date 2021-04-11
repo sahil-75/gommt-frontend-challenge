e4c4ad43d3a00279a021d1e502e2b225939553b0
sahil-75 <sahil.kalyani75@gmail.com>
2021-04-12 00:37:11 +0530

    Clearing Login form fields after logging in

--- Sahil_GoMMT/app/Screens/Login.js
+++ Sahil_GoMMT/app/Screens/Login.js
@@ -25,6 +25,10 @@ export default class Splash extends Component {
         //console.log(this.state.username, this.state.password);
         if (this.state.username === 'sahil_75' && this.state.password === 'GoMMT@123') {
             AsyncStorage.setItem('isLoggedIn', 'Y');
+            this.setState({
+                username: '',
+                password: ''
+            });
             navigate('Home');
         } else {
             Alert.alert('Invalid Username and/or Password. Try Again!');
@@ -36,8 +40,8 @@ export default class Splash extends Component {
             <SafeAreaView style={[styles.container, { alignItems: 'center' }]}>
                 <Image style={{ width: (width / 1.5), marginBottom: 0, height: 100 }} source={require('../assets/images/banner.png')} resizeMode='contain' />
                 <View style={{ alignItems: 'center', padding: 20 }}>
-                    <TextInput onChangeText={(text) => this.setState({ username: text })} placeholder='Username / Email Id' placeholderTextColor={ColorTheme.whiteFaded} style={[styles.box, styles.textInput, { width: 250 }]} />
-                    <TextInput onChangeText={(text) => this.setState({ password: text })} placeholder='Password' secureTextEntry={true} placeholderTextColor={ColorTheme.whiteFaded} style={[styles.box, styles.textInput, { width: 250 }]} />
+                    <TextInput value={this.state.username} onChangeText={(text) => this.setState({ username: text })} placeholder='Username / Email Id' placeholderTextColor={ColorTheme.whiteFaded} style={[styles.box, styles.textInput, { width: 250 }]} />
+                    <TextInput value={this.state.password} onChangeText={(text) => this.setState({ password: text })} placeholder='Password' secureTextEntry={true} placeholderTextColor={ColorTheme.whiteFaded} style={[styles.box, styles.textInput, { width: 250 }]} />
                     <TouchableOpacity style={[styles.button, { opacity: (this.state.username.length == 0 || this.state.password.length == 0) ? 0.5 : 1 }]} activeOpacity={0.5} onPress={() => this.validateLogin()} disabled={(this.state.username.length == 0 || this.state.password.length == 0)}>
                         <Text style={[styles.h4, { color: ColorTheme.primary }]}>LOGIN</Text>
                     </TouchableOpacity>
